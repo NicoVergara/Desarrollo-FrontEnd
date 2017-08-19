9569ae5c2affe4e11d9eba16a6ae7d061dfb0c93
NicoVergara <nicolasvergara89@gmail.com>
2017-08-19 14:03:54 -0300

    Agregando comentario para el primer ejercicio.

--- Clase-01/main.js
+++ Clase-01/main.js
@@ -1,3 +1,82 @@
+//Primer Clase: Nivelación. Let y Const.
+// Let: Agregar información.
+if (true) {
+  let name = 'Nicolas';
+  console.log(name);
+}
+
+let i = 0;
+for (; i < 10; i++) {
+  console.log(i);
+}
+console.log('valor final de i es:', i);
+
+// Let en funciones: Agregar información.
+function ejemplo1() {
+  let color = 'rojo';
+
+  if (true) {
+    let color = 'amarillo';
+    console.log('subBloque 1 color:', color);
+  }
+
+  console.log('ejemplo 1 color:', color);
+}
+ejemplo1();
+
+// Const: Agregar información.
+var name = 'Juan';
+console.log('Mi nombre es: ', name);
+
+name = 'Pablo';
+
+console.log('Mi nuevo nombre es: ', name);
+
+// Objetos
+const obj = { frutas: { manzana: { color: 'rojo' } } };
+console.log('El color de la fruta es: ', obj.frutas.manzana.color);
+
+obj.frutas.manzana.color = 'verde';
+
+console.log('Mi nuevo color de fruta es: ', obj.frutas.manzana.color);
+
+// Concatenación: Strings Templates.
+let nombre = 'Nicolás';
+console.log(`¡Hola!
+Mi nombre es ${nombre}`);
+
+// Crear una función y pasarle por parámetro un nombre.
+function getName(name) {
+  return name;
+}
+let str = `¡Hola! Soy Desarrollador FrontEnd
+Mi nombre es ${getName('Nicolás')}`;
+
+console.log(str);
+
+// Array: Método Find.
+let numeros = [2, 30, 1, 5, 10];
+let resultado = numeros.find(function(x) {
+  return x % 10 == 0;
+});
+console.log('El primer número que cumple la condición es: ', resultado);
+
+let nombres = ['Alan', 'Zoe', 'Ana', 'Jose'];
+let resultado2 = nombres.find(function(nombre) {
+  return nombre.length == 3;
+});
+
+console.log('El nombre que cumple la condición es: ' + resultado2);
+
+// Declarar un array sin elementos en los índices 2, 3 y 4
+var a = [0, 1, , , , 5, 6];
+
+// Muestra todos los índices, no sólo aquellos que tienen valores asignados
+a.find(function(value, index) {
+  console.log('Indice: ' + index + ' valor ' + value);
+});
+
+// Recorrer el html y cambiar el style de la clase 'ACTIVE'.
 let lista = document.getElementsByTagName('li');
 Array.from(lista).find(function(node) {
   if (node.className == 'active') {
