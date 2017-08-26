21832a7358d896bb8977bb64ba8807051138cd2d
NicoVergara <nicolasvergara89@gmail.com>
2017-08-26 15:50:04 -0300

    Mostrando mi nuevo componente.

--- Clase-02/react-primerospasos/src/index.js
+++ Clase-02/react-primerospasos/src/index.js
@@ -2,7 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Button from './components/button/Button';
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Button
+    value="Enviar"
+    color="success"
+    event={() => {
+      alert('¡Enviado!');
+    }}
+  />,
+  document.getElementById('root2')
+);
+
+ReactDOM.render(
+  <Button
+    value="Cancelar"
+    color="cancel"
+    event={() => {
+      alert('¡Cancelado!');
+    }}
+  />,
+  document.getElementById('root3')
+);
 registerServiceWorker();
